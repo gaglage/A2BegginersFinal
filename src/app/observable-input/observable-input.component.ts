da6c88bf094d244d59c360e45228a4a8f6171c69
gaglage <gabialvarez@gmail.com>
2017-04-14 12:09:48 +0200

    observable

--- src/app/observable-input/observable-input.component.ts
+++ src/app/observable-input/observable-input.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/mergeMap';
 
 @Component({
   selector: 'app-observable-input',
@@ -10,17 +15,25 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class ObservableInputComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: Http) {
     this.form = this.fb.group({
       search: []
     });
     let search = this.form.get('search');
 
     search.valueChanges
-      .auditTime(400)// debounceTime
+      .debounceTime(400)// debounceTime
       .filter(text => text.length >= 3)
-      .map(str => (<string>str).replace(' ', '-'))
-      .subscribe(x => console.log(x))
+      .mergeMap (searchTerm => {
+        let url = `https://api.spotify.com/v1/search?type=artist&q=${searchTerm}`;
+        return http.get(url)
+          .toPromise()
+          .then(response => response.json());
+
+      })
+      .subscribe(artist => console.log(artist))
+      // .unsubscribe();
+console.log(new Observable());
 
   }
 
