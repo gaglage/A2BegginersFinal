8ed4246c5529a711e2b8e51ccb966969a3c8830a
gaglage <gabialvarez@gmail.com>
2017-06-04 10:41:15 +0200

    observable

--- src/app/observable-input/observable-input.component.ts
+++ src/app/observable-input/observable-input.component.ts
@@ -6,9 +6,12 @@ import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/delay';
+
 import 'rxjs/add/observable/range';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/throw';
 
 @Component({
   selector: 'app-observable-input',
@@ -24,15 +27,34 @@ export class ObservableInputComponent implements OnInit {
 
     // .unsubscribe();
 
+    let userStream = Observable.of({
+      userId: 1, username: 'gabi'
+    }).delay(2000);
+    let tweetStream = Observable.of([1, 2, 3]).delay(1550);
 
+    Observable.forkJoin(userStream, twexetStream)
+      .subscribe(result => console.log(result));
 
-    Observable
-      .interval(1000)
-      .flatMap(x => {
-        console.log('Calling server');
-        return Observable.of([1, 2, 3]);
-      })
-      .subscribe(news => console.log(news));
+    Observable.forkJoin(userStream, tweetStream)
+      .map(joined => new Object({ user: joined[0], tweets: joined[1] }))
+      .subscribe(result => console.log(result));
+
+    //error
+
+    let observable = Observable.throw(new Error('Something was wrong'));
+    observable.subscribe(
+      x => console.log(x),
+      error => console.log(error)
+
+    );
+
+    // Observable
+    //   .interval(1000)
+    //   .flatMap(x => {
+    //     console.log('Calling server');
+    //     return Observable.of([1, 2, 3]);
+    //   })
+    //   .subscribe(news => console.log(news));
 
   }
 
