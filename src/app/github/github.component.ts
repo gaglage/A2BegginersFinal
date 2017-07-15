713b5478672c17a34c5329d7b36671fdccbcd910
gaglage <gabialvarez@gmail.com>
2017-07-15 11:53:43 +0200

    add material

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -4,16 +4,31 @@ import { GitHubService } from './github.service';
 
 @Component({
   selector: 'app-github',
-  templateUrl: 'github.component.html'
+  templateUrl: 'github.component.html',
+  styleUrls: ['./github.component.scss']
 })
 export class GitHubComponent implements OnInit {
-  url = 'https://api.github.com/users/octocat';
-  githubdata;
+  user = 'octocat';
+  githubUrl = 'https://github.com/'
+  githubUrlApi = 'https://api.github.com/users/'
+  url = this.githubUrlApi + this.user;
+  githubuser;
+  githubfollowers;
+  isLoaded = false;
   constructor(private _githubService: GitHubService) { }
+
+  gotoProfile(user) {
+    var newWindow = window.open(this.githubUrl+user);
+  }
   ngOnInit() {
     console.log('github');
     this._githubService.getGitHub(this.url).subscribe(
-      x => { console.log(x); }
+      x => {
+        this.githubuser = x[0];
+        this.githubfollowers = x[1];
+        this.isLoaded = true;
+        console.log(x[0]);
+      }
     );
   }
 
